27538df149735616609439d24813fed949abd8ac
Nathan Suzano <nathan.suzano@gmail.com>
2023-03-18 19:21:26 -0300

    Criação da classe para produtos de fornecedores brasileiros

---
+++ lib/src/model/supplier1.dart
@@ -0,0 +1,48 @@
+class SupplierBR {
+  String nome;
+  String descricao;
+  String categoria;
+  String imagem;
+  String preco;
+  String material;
+  String departamento;
+  String id;
+  String name;
+
+  SupplierBR(
+      {this.nome,
+      this.descricao,
+      this.categoria,
+      this.imagem,
+      this.preco,
+      this.material,
+      this.departamento,
+      this.id,
+      this.name});
+
+  SupplierBR.fromJson(Map<String, dynamic> json) {
+    nome = json['nome'];
+    descricao = json['descricao'];
+    categoria = json['categoria'];
+    imagem = json['imagem'];
+    preco = json['preco'];
+    material = json['material'];
+    departamento = json['departamento'];
+    id = json['id'];
+    name = json['name'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['nome'] = this.nome;
+    data['descricao'] = this.descricao;
+    data['categoria'] = this.categoria;
+    data['imagem'] = this.imagem;
+    data['preco'] = this.preco;
+    data['material'] = this.material;
+    data['departamento'] = this.departamento;
+    data['id'] = this.id;
+    data['name'] = this.name;
+    return data;
+  }
+}
