e0a3890e580c93c2fffbc316dde5a5675183e5ed
Nathan Suzano <nathan.suzano@gmail.com>
2023-03-21 13:59:34 -0300

    Tela de Total de produtos

---
+++ lib/src/widgets/cart_total.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce_app/controller/cart_controller.dart';
+import 'package:get/get.dart';
+
+class CartTotal extends StatelessWidget {
+  final CartController controller = Get.find();
+
+  @override
+  Widget build(BuildContext context) {
+    print(controller.products);
+    return Obx(
+      () => Container(
+        padding: EdgeInsets.symmetric(horizontal: 75),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Total",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              controller.total.toString() == "()"
+                  ? "00.00"
+                  : "${controller.total}",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
