66008a72e8a1b4c12f0fe0bb2f7e0efe4e46e999
Nathan Suzano <nathan.suzano@gmail.com>
2023-03-23 18:47:00 -0300

    Melhorado o menu Adicionado um Drawer e uma so função

--- lib/src/pages/mainPage.dart
+++ lib/src/pages/mainPage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce_app/src/pages/cart_screen.dart';
 import 'package:flutter_ecommerce_app/src/pages/home_page.dart';
+import 'package:flutter_ecommerce_app/src/pages/sell_products.dart';
 import 'package:flutter_ecommerce_app/src/pages/shopping_cart_page.dart';
 import 'package:flutter_ecommerce_app/src/themes/light_color.dart';
 import 'package:flutter_ecommerce_app/src/themes/theme.dart';
@@ -20,6 +21,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   bool isHomePageSelected = true;
+
   Widget _appBar() {
     return Container(
       padding: AppTheme.padding,
@@ -63,7 +65,9 @@ class _MainPageState extends State<MainPage> {
         icon,
         color: color,
       ),
-    ).ripple(() {}, borderRadius: BorderRadius.all(Radius.circular(13)));
+    ).ripple(() {
+      Get.to(SellProducts());
+    }, borderRadius: BorderRadius.all(Radius.circular(13)));
   }
 
   Widget _title() {
@@ -114,9 +118,115 @@ class _MainPageState extends State<MainPage> {
     }
   }
 
+  Widget _menuItem(int id, String title, IconData icon) {
+    return Material(
+      child: InkWell(
+        onTap: () {
+          Get.to(SellProducts());
+        },
+        child: Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Icon(
+                  icon,
+                  size: 20,
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                flex: 3,
+                child: Text(title),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _myDrawerList() {
+    return Container(
+      padding: EdgeInsets.only(top: 15),
+      child: Column(
+        children: [_menuItem(1, "Total Vendas", Icons.list_alt_outlined)],
+      ),
+    );
+  }
+
+  Widget _myHeadDrawer() {
+    return Container(
+      color: Colors.grey[400],
+      width: double.infinity,
+      height: 200,
+      padding: EdgeInsets.only(top: 20.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            height: 70,
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  image: AssetImage("assets/user.png"),
+                )),
+          ),
+          Text(
+            "Texto",
+            style: TextStyle(color: Colors.black, fontSize: 20),
+          ),
+          Text(
+            "asdas@asdasdsa",
+            style: TextStyle(color: Colors.black54, fontSize: 14),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100, // Set this height
+        actions: [
+          Image.asset("assets/user.png")
+          // Container(
+          //   height: 120.0,
+          //   width: 120.0,
+          //   decoration: BoxDecoration(
+          //     borderRadius: BorderRadius.circular(14),
+          //     image: DecorationImage(
+          //       image: AssetImage('assets/user.png'),
+          //       fit: BoxFit.fill,
+          //     ),
+          //   ),
+          // )
+          // Container(
+          //     decoration: BoxDecoration(
+          //       border: Border.all(width: 5),
+          //       borderRadius: BorderRadius.circular(20), //<-- SEE HERE
+          //     ),
+          //     child: Image.asset("assets/user.png")),
+        ],
+        backgroundColor: Color(0xfffbfbfb),
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Container(
+            child: Column(
+              children: [
+                _myHeadDrawer(),
+                _myDrawerList(),
+              ],
+            ),
+          ),
+        ),
+      ),
       body: SafeArea(
         child: Stack(
           fit: StackFit.expand,
@@ -137,7 +247,7 @@ class _MainPageState extends State<MainPage> {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
-                    _appBar(),
+                    // _appBar(),
                     _title(),
                     Expanded(
                       child: AnimatedSwitcher(
