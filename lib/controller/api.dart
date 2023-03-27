63d761a5b511f2bb1d6fccc23819743bbf210695
Nathan Suzano <nathan.suzano@gmail.com>
2023-03-27 16:48:38 -0300

    Função retorna produtos Europeus
    Adicionado função que retorna os produtos europeus da api.

--- lib/controller/api.dart
+++ lib/controller/api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter_ecommerce_app/src/model/supplier1.dart';
+import 'package:flutter_ecommerce_app/src/model/supplier2.dart';
 import 'package:http/http.dart' as http;
 
 class Api {
@@ -13,6 +14,20 @@ class Api {
     if (response.statusCode == 200) {
       List data = jsonDecode(response.body);
       return data.map((json) => SupplierBR.fromJson(json)).toList();
+    } else {
+      throw Exception('Erro não foi possível carregar a lista');
+    }
+  }
+
+  Future<List<SupplierEU>> produtosEuLista() async {
+    List<String> produtos = [];
+
+    Uri url = Uri.parse(
+        'http://616d6bdb6dacbb001794ca17.mockapi.io/devnology/european_provider');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      List data = jsonDecode(response.body);
+      return data.map((json) => SupplierEU.fromJson(json)).toList();
     } else {
       throw Exception('Erro não foi possível carregar a lista');
     }
