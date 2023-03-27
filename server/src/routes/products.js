4edd1752c8aacc27c8aa0681858f4422b779e3e2
Nathan Suzano <nathan.suzano@gmail.com>
2023-03-26 23:48:16 -0300

    Adicionado novas funções para a API.
    Para get retorna lista de produtos, clientes, id do ultimo produto, pedidos.
    
    Para post retorna produtos, clientes, pedidos.
    
    alguns velhos estão comentados.

--- server/src/routes/products.js
+++ server/src/routes/products.js
@@ -7,8 +7,10 @@ router.get('/', (req, res) => {
     res.status(200).json('Server on port 8000 and database is connected');
 });
 
-router.get('/:products', (req, res) => {
-    mysqlConnection.query('select * from products;', (error, rows, fields) => {
+//GET
+
+router.get('/supplierbr', (req, res) => {
+    mysqlConnection.query('select * from supplier_br;', (error, rows, fields) => {
         if(!error){
             res.json(rows);
         }
@@ -18,9 +20,83 @@ router.get('/:products', (req, res) => {
     });
 });
 
-router.get('/:products/:id', (req, res) => {
+router.get('/products', (req, res) => {
     const {id} = req.params;
-    mysqlConnection.query('select * from user where id = ?;', [id], (error, rows, fields) => {
+
+    mysqlConnection.query('select * from products;', [id], (error, rows, fields) => {
+        if(!error){
+            res.json(rows);
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
+
+router.get('/lastproducts', (req, res) => {
+
+    mysqlConnection.query('select id_supplier_br from supplier_br ORDER BY id_supplier_br DESC LIMIT 1;', (error, rows, fields) => {
+        if(!error){
+            res.json(rows);
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
+
+router.get('/orders/:id', (req, res) => {
+    const {id} = req.params;
+
+    mysqlConnection.query('select * from order_details where id_clients = ?;', [id], (error, rows, fields) => {
+        if(!error){
+            res.json(rows);
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
+
+router.get('/clients', (req, res) => {
+    mysqlConnection.query('select * from clients;', (error, rows, fields) => {
+        if(!error){
+            res.json(rows);
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
+
+router.get('/clients/:id', (req, res) => {
+    const {id} = req.params;
+
+    mysqlConnection.query('select * from clients where id_clients = ?;', [id], (error, rows, fields) => {
+        if(!error){
+            res.json(rows);
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
+
+router.get('/supplier_br/:id', (req, res) => {
+    const {id} = req.params;
+    mysqlConnection.query('select * from supplier_br where id_supplier_br = ?;', [id], (error, rows, fields) => {
+        if(!error){
+            res.json(rows);
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
+
+router.get('/supplier_eu/:id', (req, res) => {
+    const {id} = req.params;
+    mysqlConnection.query('select * from supplier_eu where id_supplier_eu = ?;', [id], (error, rows, fields) => {
         if(!error){
             res.json(rows);
         }
@@ -31,12 +107,13 @@ router.get('/:products/:id', (req, res) => {
 });
 
 
-router.post('/:products', (req, res) => {
-    const {id, nome, preco} = req.body;
+// POST
+router.post('/productbr', (req, res) => {
+    const {nome_supplier_br, preco_supplier_br, descricao_supplier_br, categoria_supplier_br, imagem_supplier_br, material_supplier_br, departamento_supplier_br} = req.body;
     console.log(req.log);
-    mysqlConnection.query('INSERT INTO products(id, nome, preco) values(?,?,?);', [id, nome, preco], (error, rows, fields) => {
+    mysqlConnection.query('INSERT INTO supplier_br(nome_supplier_br, preco_supplier_br, descricao_supplier_br, categoria_supplier_br, imagem_supplier_br, material_supplier_br, departamento_supplier_br) values(?,?,?,?,?,?,?);', [nome_supplier_br, preco_supplier_br, descricao_supplier_br, categoria_supplier_br, imagem_supplier_br, material_supplier_br, departamento_supplier_br], (error, rows, fields) => {
         if(!error){
-            res.json({Status: 'User Saved'});
+            res.json({Status: 'Product Saved'});
         }
         else {
             console.log(error);
@@ -44,31 +121,70 @@ router.post('/:products', (req, res) => {
     });
 });
 
-router.put('/:products/:id', (req, res) => {
-    const {id, nome, preco} = req.body;
-    console.log(req.log);
-    mysqlConnection.query('update products set nome = ?, preco = ?;', [nome, preco], (error, rows, fields) => {
+router.post('/producteu', (req, res) => {
+    const {name_product, description_product, details_product, discont_produto, hasdiscont_product, price_produto} = req.body;
+    console.log(req.log);l
+    mysqlConnection.query('INSERT INTO supplier_eu(name_supplier_eu, description_supplier_eu, details_supplier_eu, discontvalue_supplier_eu, has_discount_supplier_eu, price_supplier_eu) values(?,?,?,?,?,?);', [name_product, description_product, details_product, discont_produto, hasdiscont_product, price_produto], (error, rows, fields) => {
         if(!error){
-            res.json({Status: 'User updated'});
+            res.json({Status: 'Product Saved'});
         }
         else {
             console.log(error);
-        } 
+        }
+    });
+});
+
+router.post('/clients', (req, res) => {
+    const {nome_clients, email_clients, senha_clients} = req.body;
+    console.log(req.log);
+    mysqlConnection.query('INSERT INTO clients(nome_clients, email_clients, senha_clients) values(?,?,?);', [nome_clients, email_clients, senha_clients], (error, rows, fields) => {
+        if(!error){
+            res.json({Status: 'Client Saved'});
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
+
+router.post('/order', (req, res) => {
+    const {total_order, quant_order, id_client, id_supplierbr, id_suppliereu} = req.body;
+    console.log(req.log);
+    mysqlConnection.query('INSERT INTO order_details(total_order_details, quant_order_details, id_clients, id_supplier_br, id_supplier_eu) values(?,?,?,?,?);', [total_order, quant_order, id_client, id_supplierbr, id_suppliereu], (error, rows, fields) => {
+        if(!error){
+            res.json({Status: 'Order Saved'});
+        }
+        else {
+            console.log(error);
+        }
+    });
+});
+
+// router.put('/:products/:id', (req, res) => {
+//     const {id, nome, preco} = req.body;
+//     console.log(req.log);
+//     mysqlConnection.query('update products set nome = ?, preco = ?;', [nome, preco], (error, rows, fields) => {
+//         if(!error){
+//             res.json({Status: 'User updated'});
+//         }
+//         else {
+//             console.log(error);
+//         } 
     
-    });
-});
+//     });
+// });
 
-router.delete('/:products/:id', (req, res) => {
-    const {id} = req.params;
-    mysqlConnection.query('delete from products where id = ?;', [id], (error, rows, fields) => {
-        if(!error){
-            res.json({Status: 'User deleted'});
-        }
-        else {
-            console.log(error);
-        } 
-    });
+// router.delete('/:products/:id', (req, res) => {
+//     const {id} = req.params;
+//     mysqlConnection.query('delete from products where id = ?;', [id], (error, rows, fields) => {
+//         if(!error){
+//             res.json({Status: 'User deleted'});
+//         }
+//         else {
+//             console.log(error);
+//         } 
+//     });
 
-});
+// });
 
 module.exports = router;
