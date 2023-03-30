bacb121e3fad8f3a945c5f38d1bc0f28ad9e3d72
Nathan Suzano <nathan.suzano@gmail.com>
2023-03-30 17:15:58 -0300

    Deletado arquivos que não eram utilizados

--- lib/controller/di.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_ecommerce_app/controller/cart_controller.dart';
-import 'package:get/get.dart';
-
-class Di {
-  Future<void> init() async {
-    Get.put(CartController());
-  }
-}
